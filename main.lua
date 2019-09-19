30b225fc7619d83f59571a152fea6850002a1093
rmdecano <52554789+rmdecano@users.noreply.github.com>
2019-09-19 21:11:29 +0800

    Add files via upload

---
+++ main.lua
@@ -0,0 +1,56 @@
+
+require 'src/Dependencies'
+
+function love.load()
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+    love.graphics.setFont(gFonts['medium'])
+    love.window.setTitle('Super 50 Bros.')
+
+    math.randomseed(os.time())
+    
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+        fullscreen = false,
+        vsync = true,
+        resizable = true
+    })
+
+    gStateMachine = StateMachine {
+        ['start'] = function() return StartState() end,
+        ['play'] = function() return PlayState() end
+    }
+    gStateMachine:change('start')
+
+    gSounds['music']:setLooping(true)
+    gSounds['music']:setVolume(0.7)
+    gSounds['music']:play()
+
+    love.keyboard.keysPressed = {}
+end
+
+function love.resize(w, h)
+    push:resize(w, h)
+end
+
+function love.keypressed(key)
+    if key == 'escape' then
+        love.event.quit()
+    end
+
+    love.keyboard.keysPressed[key] = true
+end
+
+function love.keyboard.wasPressed(key)
+    return love.keyboard.keysPressed[key]
+end
+
+function love.update(dt)
+    gStateMachine:update(dt)
+
+    love.keyboard.keysPressed = {}
+end
+
+function love.draw()
+    push:start()
+    gStateMachine:render()
+    push:finish()
+end
